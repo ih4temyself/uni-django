375f9e521a537e9779ea422f1257105e86201c80
ih4temyself <dyakonenko123@gmail.com>
2024-10-08 16:01:00 +0300

    byebye

--- backend/bookmarks/urls.py
+++ backend/bookmarks/urls.py
@@ -1,12 +1,11 @@
+from bookmarks.views import BookmarkViewSet, CategoryViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from bookmarks.views import BookmarkViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r"bookmarks", BookmarkViewSet, basename="bookmark")
 router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
 ]
--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/", include("bookmarks.urls")),
+    path("", include("bookmarks.urls")),
     path("admin/", admin.site.urls),
 ]
